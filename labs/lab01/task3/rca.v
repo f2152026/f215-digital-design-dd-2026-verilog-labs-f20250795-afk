3bc1a5bf76027380bce22fa70ff1f4f1e2893bc4
f20250795-afk <f20250795@pilani.bits-pilani.ac.in>
2026-08-29 15:39:19 +0000

    lab01 task3

--- labs/lab01/task3/rca.v
+++ labs/lab01/task3/rca.v
@@ -24,40 +24,16 @@ module rca(
 
   wire c1, c2, c3;
 
-  // Four FA_Gate instances connected in a ripple-carry chain
-  FA_Gate FA0 (
-    .a    (a[0]),
-    .b    (b[0]),
-    .cin  (cin),
-    .sum  (sum[0]),
-    .cout (c1)
-  );
+  // Instantiate the 4 full adders using named port connections
+  FA_Gate FA0 (.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c1));
+  FA_Gate FA1 (.a(a[1]), .b(b[1]), .cin(c1),  .sum(sum[1]), .cout(c2));
+  FA_Gate FA2 (.a(a[2]), .b(b[2]), .cin(c2),  .sum(sum[2]), .cout(c3));
+  FA_Gate FA3 (.a(a[3]), .b(b[3]), .cin(c3),  .sum(sum[3]), .cout(cout));
 
-  FA_Gate FA1 (
-    .a    (a[1]),
-    .b    (b[1]),
-    .cin  (c1),
-    .sum  (sum[1]),
-    .cout (c2)
-  );
+endmodule
 
-  FA_Gate FA2 (
-    .a    (a[2]),
-    .b    (b[2]),
-    .cin  (c2),
-    .sum  (sum[2]),
-    .cout (c3)
-  );
 
-  FA_Gate FA3 (
-    .a    (a[3]),
-    .b    (b[3]),
-    .cin  (c3),
-    .sum  (sum[3]),
-    .cout (cout)
-  );
-
-  module FA_Gate(
+module FA_Gate(
   input  a,
   input  b,
   input  cin,
